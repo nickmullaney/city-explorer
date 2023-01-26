cf335166419d6d614d701d950fb2abd22ac1d5d2
nickmullaney <nick.mullaney@gmail.com>
2023-01-25 19:05:43 -0800

    step 1 complete

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Footer extends React.Component{
+  render(){
+    return(
+      <p>© City Explorer 2023 </p>
+    )
+  }
+}
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <h3>Find out what's near you!</h3>
+      </div>
+    )
+  }
+}
