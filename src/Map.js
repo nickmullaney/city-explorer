bf70a1d726a4ea9db31d561f9febe4b2bdd6069f
nickmullaney <nick.mullaney@gmail.com>
2023-01-26 21:19:29 -0800

    build me

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
+
 class Map extends React.Component{
   render(){
     return(
       <Container className="text-center">
-        <img src = {`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12`} alt ={'Map of ${this.state.cityData.display_name}'}/>
-        {console.log(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12`)} 
+      
+        <img src = {`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12`} alt ={`Map of ${this.state.cityData.display_name}`}/>
       </Container>
     )
   }
-  
+
 }
 
 export default Map;
