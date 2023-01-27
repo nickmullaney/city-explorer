bdc1fca08e0718ed4fe373dad10a9ab891eee078
nickmullaney <nick.mullaney@gmail.com>
2023-01-26 21:10:21 -0800

    build!

---
+++ build/precache-manifest.fb81b23de1e0488e83c107a127791e70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "30883f7799d5e701ac8c",
+    "url": "/static/css/main.406b4b92.chunk.css"
+  },
+  {
+    "revision": "30883f7799d5e701ac8c",
+    "url": "/static/js/main.7e5e41d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36d721173755e235999c",
+    "url": "/static/css/2.ff110495.chunk.css"
+  },
+  {
+    "revision": "36d721173755e235999c",
+    "url": "/static/js/2.88c65110.chunk.js"
+  },
+  {
+    "revision": "a7b0cdeb55e5ca07219c2aa22010df31",
+    "url": "/static/media/cityError.a7b0cdeb.jpg"
+  },
+  {
+    "revision": "26d90d0f851a5f80d384666995cadd32",
+    "url": "/index.html"
+  }
+];
--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,6 @@ class Map extends React.Component{
   render(){
     return(
       <Container className="text-center">
-        {/* <img src={this.props.mapImage} alt={this.props.city} height={500}/>   */}
         <img src = {`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12`} alt ={'Map of ${this.state.cityData.display_name}'}/>
         {console.log(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.props.lat},${this.props.lon}&zoom=12`)} 
       </Container>
